1d2a906e2a9d31f8f981ea275a257cfc4c8a91c8
mchenf <mochen@foss.dk>
2023-09-29 16:34:13 +0200

    Added a base project for testing

---
+++ Foss.PowerShell.MailObfu/Foss.PowerShell.MailObfu/MailObfu/HideMailContent.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+
+namespace MailObfu
+{
+    [Cmdlet(VerbsCommon.Hide, "MailContent")]
+    public class HideMailContent : Cmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "ByContent")]
+        public string? Content { get; set; }
+
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "ByDirectory")]
+        public string? Directory { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (Content == null)
+            {
+                WriteObject("ParmSet: ByDirectory is used.");
+            }
+            else
+            {
+                WriteObject("ParmSet: ByContent is used.");
+            }
+        }
+    }
+}
